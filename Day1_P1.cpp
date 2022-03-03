5ae007d99509b175d19693523ae1a6ec667573a3
praval_ss <pravalgopalp2@gmail.com>
2022-03-03 22:00:49 +0530

    03.03.22

---
+++ Day1_P1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<pair<int,int>> vp;
+        for (int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    for(int kr=0;kr<m;kr++){
+                        vp.push_back({kr,j});
+                    }
+                    for(int kc=0;kc<n;kc++){
+                        vp.push_back({i,kc});
+                    }
+                }
+            }
+        }
+        
+        for(auto el:vp){
+            matrix[el.first][el.second]=0;
+        }
+    }
+};
